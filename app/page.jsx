20f225aaadf87ba66cf7ad3ea87f5c3aa20b2d30
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-03-10 17:01:41 -0300

    Refactor: Login

--- app/page.jsx
+++ app/page.jsx
@@ -4,17 +4,26 @@ import Input from '../components/Input';
 import { useRouter } from "next/navigation"
 
 
-export default function Home() {
+export default function Registro() {
     const router = useRouter();
 
-    return(
+    return (
         <div className={styles.container}>
-            <div className={styles.login}>
-                <h1>Cadastre se</h1>
-                <Input text="Nome" tipo="text" id="nome"/>
-                <Input text="Email" tipo="email" id="email"/>
-                <Input text="Senha" tipo="password" id="senha"/>
-                <button className={styles.button} onClick={() => router.push("/perfil")}>Login</button>
+            <div className={styles.imageContainer}>
+                <img src="/baleia.jpg" alt="Logo" />
+            </div>
+
+            <div className={styles.formularioContainer}>
+                <h1 className={styles.title}>Um Nome Bem Top</h1>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+            
+            <Input text="Nome" tipo="text" id="nome" />
+            <Input text="Email" tipo="email" id="email" />
+            <Input text="Senha" tipo="password" id="senha" />
+            <button className={styles.button} onClick={() => router.push("/perfil")}>Login</button>
+            <p className={styles.link} onClick={() => router.push("/login")}>
+                    Já tenho uma conta
+                </p>
             </div>
         </div>
     )
